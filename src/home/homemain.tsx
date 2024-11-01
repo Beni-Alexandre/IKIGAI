9347014c192447e16ec9bcd5924123d10a2204bf
HONFOGA Beni <honfogabeni@gmail.com>
2024-11-01 08:25:25 +0000

    homamain cahnges

--- src/home/homemain.tsx
+++ src/home/homemain.tsx
@@ -4,18 +4,25 @@ import { Button } from "@/components/ui/button";
 
 function Homemain() {
     return (
-        <section className="bg-blue-600 text-white">
-            <div className="container mx-auto px-4 py-20 text-center"></div>
-            <h1 className="mb-6 text-4xl font-extrabold leading-tight lg:text-5xl" >
-                Discover Products That Match Your Moment
-            </h1>
-            <p className="mb-10 text-xl">
-                Ikigai brings you the most relevant and in-demand items, perfectly aligned with current trends and seasons.
-            </p>
-            <Button size="lg" variant="secondary">
-                Explore Our Collection
-            </Button>
-        </section >
+        <div>
+            <section className="bg-blue-600 text-white">
+                <div className="container mx-auto px-4 py-20 text-center"></div>
+                <h1 className="mb-6 text-4xl font-extrabold leading-tight lg:text-5xl" >
+                    Discover Products That Match Your Moment
+                </h1>
+                <p className="mb-10 text-xl">
+                    Ikigai brings you the most relevant and in-demand items, perfectly aligned with current trends and seasons.
+                </p>
+                <Button size="lg" variant="secondary">
+                    Explore Our Collection
+                </Button>
+            </section >
+            <section>
+                <h1>Why Choose IKIGAI</h1>
+
+            </section>
+
+        </div>
 
 
     )

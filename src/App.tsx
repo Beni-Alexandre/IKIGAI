7203de2075f94c58095e206f47be6e027c4fd174
Beni-Alexandre <honfogabeni@gmail.com>
2024-11-05 13:59:24 +0000

    no comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 
 import Home from "./home/home"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-
 import About from "./aboutpage/about"
 import Contact from "./Contactpage/contact"
 import Products from "./productpage/product"
 
 
-
-
 function App() {
 
-
   return (
+
     <>
+
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<Products />} />

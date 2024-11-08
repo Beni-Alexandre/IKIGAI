2ec385e44074d82eebbb4833ec7a8154266c7b1b
Beni-Alexandre <honfogabeni@gmail.com>
2024-11-08 15:46:52 +0000

    product's card added

--- src/productpage/product.tsx
+++ src/productpage/product.tsx
@@ -1,4 +1,14 @@
 import Header from "@/home/header";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+
 
 
 function Products() {
@@ -7,8 +17,22 @@ function Products() {
             <Header />
             <main className="container mx-auto px-4 py-12" >
                 <h1 className="mb-8 text-4xl font-bold text-center">Products</h1>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Product name</p>
+                    </CardContent>
+                    <CardFooter>
+                        <Button className="text-blue-600" variant="outline">Buy Now</Button>
+
+                    </CardFooter>
+                </Card>
+
             </main>
         </div>
     )
 }
-export default Products;
+export default Products; 

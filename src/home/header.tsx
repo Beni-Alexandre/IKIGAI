93c1f5b343fa83b3513b05548a71edb618e4a8ed
Beni-Alexandre <honfogabeni@gmail.com>
2024-11-15 00:11:24 +0000

    responsivity of the web app

--- src/home/header.tsx
+++ src/home/header.tsx
@@ -1,33 +1,37 @@
 
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-// import { Menu } from 'lucide-react';
-// import { useState } from "react";
+import { Menu } from 'lucide-react';
+import { useState } from "react";
 
 
 
 function Header() {
 
-
+    const [showNavbar, setShowNavbar] = useState(false);
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar);
+    }
 
     return (
         <div>
             <header className="bg-white shadow-sm">
                 <nav>
                     <div className="text-2xl font-bold text-blue-600">Ikigai</div>
-                    {/* <div className="logo">
-                        <img src="images/ikigai lg.jpg" alt="" />
-                    </div> */}
+
 
                     <div className="space-x-4">
-                        {/* <Button >   <Menu /></Button> */}
-                        <ul className="menu">
-                            <Link to='/'> <Button variant="ghost">HOME</Button></Link>
+                        <div className="menu-icon" onClick={handleShowNavbar} >
+                            <Menu /></div>
+                        <div className={`menu ${showNavbar && "active"}`}>
+                            <ul>
+                                <Link to='/'> <Button variant="ghost">HOME</Button></Link>
 
-                            <Link to="/about"> <Button variant="ghost">ABOUT-US</Button></Link>
-                            <Link to="/product">         <Button variant="ghost">PRODUCTS</Button></Link>
-                            <Link to="/contact">  <Button variant="ghost">CONTACT-US</Button></Link>
-                        </ul>
+                                <Link to="/about"> <Button variant="ghost">ABOUT-US</Button></Link>
+                                <Link to="/product">         <Button variant="ghost">PRODUCTS</Button></Link>
+                                <Link to="/contact">  <Button variant="ghost">CONTACT-US</Button></Link>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </header>

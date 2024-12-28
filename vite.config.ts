053b06637085f5dc5b7a421c697cea147070cf63
Beni-Alexandre <honfogabeni@gmail.com>
2024-12-28 18:40:28 +0100

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, 'src')  // Resolve '@' to the 'src' folder
+    }
   },
-})
+  server: {
+    port: 5173  // Ensure Vite is running on the correct port
+  }
+});
